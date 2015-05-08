2921ea64aba24c19eb762f04d5871fc3da46146c
sundhar-repo <sundhar910@gmail.com>
2015-05-08 20:34:49 +0530

    Added left menu and right menus.

--- src/com/halo/casino/MainTemplate.java
+++ src/com/halo/casino/MainTemplate.java
@@ -6,42 +6,40 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
+import android.view.View.OnClickListener;
 
-public class MainTemplate extends FragmentActivity{
-	private String[] mPlanetTitles = {"1","10","5","15","25","6","30","17","11","4","19","8",};
+public class MainTemplate extends FragmentActivity implements OnClickListener{
+	//private String[] mPlanetTitles = {"1","10","5","15","25","6","30","17","11","4","19","8",};
 	private DrawerLayout mDrawerLayout;
-    private GridView mLeftDrawerList, mRightDrawerList;
+    private View mLeftDrawerList, mRightDrawerList;
 	
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.main_template);
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mLeftDrawerList = (GridView) findViewById(R.id.left_drawer);
-        mRightDrawerList = (GridView) findViewById(R.id.right_drawer);
+		mLeftDrawerList = (View) findViewById(R.id.left_drawer);
+        mRightDrawerList = (View) findViewById(R.id.right_drawer);
         
         // Set the adapter for the list view
-        mLeftDrawerList.setAdapter(new ArrayAdapter<String>(this,
+        /*mLeftDrawerList.setAdapter(new ArrayAdapter<String>(this,
         		android.R.layout.simple_list_item_1, mPlanetTitles));
         mRightDrawerList.setAdapter(new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_1, mPlanetTitles));
+        		android.R.layout.simple_list_item_1, mPlanetTitles));*/
         // Set the list's click listener
-        mLeftDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-        mRightDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+       /* mLeftDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mRightDrawerList.setOnItemClickListener(new DrawerItemClickListener());*/
         
         Fragment fragment = new HomeFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                        .replace(R.id.content_frame, fragment, "home_fragment_tag")
                        .commit();
-    }
+     }
 	
 
 	
-	private class DrawerItemClickListener implements GridView.OnItemClickListener {
+	/*private class DrawerItemClickListener implements GridView.OnItemClickListener {
 	    @Override
 	    public void onItemClick(AdapterView parent, View view, int position, long id) {
 	        //selectItem(position);
@@ -63,5 +61,44 @@ public class MainTemplate extends FragmentActivity{
                 //mDrawerLayout.openDrawer(Gravity.END);
             }
 	    }
+	}*/
+
+
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.left_menu_btn:
+			//open Left menu drawer: true - open/ close left menu
+			 openOrCloseDrawer(true);
+			break;
+
+		case R.id.right_menu_btn:
+			//open right menu drawer: false - open/ close right menu
+			openOrCloseDrawer(false);
+			break;
+			
+		default:
+			break;
+		}
+	}
+
+
+
+	private void openOrCloseDrawer(boolean isLeftDrawer) {
+		if(isLeftDrawer && mDrawerLayout != null && mLeftDrawerList != null){
+			if (mDrawerLayout.isDrawerOpen(mLeftDrawerList)) {
+				mDrawerLayout.closeDrawer(mLeftDrawerList);
+			}else{
+				mDrawerLayout.openDrawer(mLeftDrawerList);
+			}
+                
+		}else if(mDrawerLayout != null && mRightDrawerList != null){
+			if (mDrawerLayout.isDrawerOpen(mRightDrawerList)) {
+				mDrawerLayout.closeDrawer(mRightDrawerList);
+			}else{
+				mDrawerLayout.openDrawer(mRightDrawerList);
+			}
+		}
 	}
 }
